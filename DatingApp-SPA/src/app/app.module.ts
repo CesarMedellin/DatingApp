325d94c11d35ff6459fbfb90f1f6d43a6c4b9dfc
cesar medellin <cesar.medellin@inteek.com.mx>
2018-12-07 12:59:46 -0600

    seccion 13

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import {NgxGalleryModule} from 'ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
-
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -38,7 +38,7 @@ return localStorage.getItem('token');
 }
 
 @NgModule({
-   declarations: [
+   declarations: [ // aqui tambien se ponen los pipes aparte de componentes
       AppComponent,
       NavComponent,
       HomeComponent,
@@ -49,7 +49,8 @@ return localStorage.getItem('token');
       MembersCardComponent,
       MemberDetailComponent,
       MemberEditComponent,
-      PhotoEditorComponent
+      PhotoEditorComponent,
+      TimeAgoPipe
    ],
    imports: [
       BrowserModule,
